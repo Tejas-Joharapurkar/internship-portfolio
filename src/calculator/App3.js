0bf1db4892e7e1cae0ad5f28edacda8f966cd88d
Tejas-Joharapurkar <102129891+Tejas-Joharapurkar@users.noreply.github.com>
2023-06-21 14:01:44 +0530

    Update App3.js

--- src/calculator/App3.js
+++ src/calculator/App3.js
@@ -67,7 +67,7 @@ function App3() {
     if (!numbers.includes(lastCharacter)) {
       exp = exp.slice(0, -1);
     }
-    const ans = (0, eval)(exp).toFixed(2) + "";
+    const ans = exp;
     setResult(ans);
   }
 
